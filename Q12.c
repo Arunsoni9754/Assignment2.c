11d728fc5241bd7c8dec8df667b32b35003df11d
Arunsoni9754 <111188653+Arunsoni9754@users.noreply.github.com>
2022-08-13 23:11:03 +0530

    Create Q12.c

---
+++ Q12.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    float r,d=0;
+    printf("write the amount of INR to be convert in USD:-");
+    scanf("%f",&r);
+    d=r+76.23;
+    printf("The amount is USD is:- %.2f",d);
+    return 0;
+}
